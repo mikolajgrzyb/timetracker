532d4c7032a8088af365d29c7f4908537e0170e0
Mikołaj Grzyb <mikolaj@codequest.com>
2014-09-20 12:21:25 +0200

    add account model and association to user

---
+++ app/models/account.rb
@@ -0,0 +1,4 @@
+class Account < ActiveRecord::Base
+  belongs_to :owner, class_name: 'User'
+
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140920092306) do
+ActiveRecord::Schema.define(version: 20140920101908) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "accounts", force: true do |t|
+    t.string   "company_name"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+    t.integer  "owner_id"
+  end
+
+  add_index "accounts", ["owner_id"], name: "index_accounts_on_owner_id", using: :btree
 
   create_table "users", force: true do |t|
     t.string   "email",                  default: "", null: false
