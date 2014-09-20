70a7e23078a387ee6e9feb2f8e040cb7c673b570
Andrzej Trzaska <atrzaska2@gmail.com>
2014-09-20 17:15:07 +0200

    registration tests

---
+++ spec/support/registration_helper.rb
@@ -0,0 +1,22 @@
+require 'rails_helper'
+
+def create_registration
+      Registration.new(
+        company_name: Faker::Company.name,
+        first_name:Faker::Name.first_name,
+        last_name: Faker::Name.last_name,
+        email: Faker::Internet.email,
+        password: "password",
+        password_confirmation: "password")
+end
+
+def registration_params
+  {
+      company_name: Faker::Company.name,
+      first_name:Faker::Name.first_name,
+      last_name: Faker::Name.last_name,
+      email: Faker::Internet.email,
+      password: "password",
+      password_confirmation: "password"
+  }
+end
