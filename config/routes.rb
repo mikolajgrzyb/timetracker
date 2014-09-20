649b0efa52662ba75c409052fad616978b7f852c
Mikołaj Grzyb <mikolaj@codequest.com>
2014-09-20 11:27:02 +0200

    add blank homepage

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+
+  root to: 'time_tracker#index'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
