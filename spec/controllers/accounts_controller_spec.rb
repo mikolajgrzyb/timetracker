e7b1d109ffdc47cd1a959c81efc2f2d794b39c30
Mikołaj Grzyb <mikolaj@codequest.com>
2014-09-20 16:21:01 +0200

    fix for validations on signup

--- spec/controllers/accounts_controller_spec.rb
+++ spec/controllers/accounts_controller_spec.rb
@@ -2,11 +2,17 @@ require 'rails_helper'
 
 RSpec.describe AccountsController, :type => :controller do
 
-  describe "GET show" do
+  describe "GET index" do
     it "returns http success" do
       get :show
       expect(response).to be_success
     end
+    it 'assigns user account to @account'
+
+  end
+
+  describe "POST#invite" do
+    it 'send invitation email'
   end
 
 end
--- spec/models/account_spec.rb
+++ spec/models/account_spec.rb
@@ -3,6 +3,6 @@ require 'rails_helper'
 RSpec.describe Account, :type => :model do
 
   it 'validates uniqueness of company_name'
-
-
+  it 'adds token in account creation process'
+  it 'validates owner_id'
 end
