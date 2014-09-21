f221fa00de634e10176623f2d7cc0eb0ee5fcb8b
Mikołaj Grzyb <mikolaj@codequest.com>
2014-09-21 11:39:28 +0200

    add edit profile for user

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140920143552) do
+ActiveRecord::Schema.define(version: 20140921082010) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -54,6 +54,7 @@ ActiveRecord::Schema.define(version: 20140920143552) do
     t.string   "first_name"
     t.string   "last_name"
     t.string   "avatar"
+    t.boolean  "accept_newsletter"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
