fb3647bdd8d6da122dfd376beead9df01395252c
Andrzej Trzaska <atrzaska2@gmail.com>
2014-09-23 16:01:39 +0200

    add slug to account

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140922184646) do
+ActiveRecord::Schema.define(version: 20140923135551) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,6 +22,7 @@ ActiveRecord::Schema.define(version: 20140922184646) do
     t.datetime "updated_at"
     t.boolean  "owner"
     t.boolean  "admin"
+    t.string   "slug"
   end
 
   create_table "invitations", force: true do |t|
