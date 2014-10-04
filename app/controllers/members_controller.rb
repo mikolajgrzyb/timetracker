e528c243919db874bb655297d96c1987e2b99291
Mikołaj Grzyb <mikolaj@codequest.com>
2014-10-04 15:47:18 +0200

    refactor

--- app/controllers/members_controller.rb
+++ app/controllers/members_controller.rb
@@ -1,21 +1,22 @@
 class MembersController < ApplicationController
   before_action :find_account, only: [:index]
 
+  respond_to :json, only: [:update]
+
   def index
-    @active_members = Member.active
-    @inactive_members = Member.inactive
+    @active_members = @account.members.active
+    @inactive_members = @account.members.inactive
     @invitations = @account.invitations
     @invitation = Invitation.new
   end
 
-  def activate
-  	@member = Member.find(id: params[:member_id])
-  	@member.active = true
-  end
-
-  def deactivate
-  	@member = Member.find(id: params[:member_id])
-  	@member.active = false
+  def update
+    @member = Member.find(id: params[:member_id])
+    if @member.update_attributes active: params[:active]
+      head :no_content
+    else
+      render json: @member.errors.full_messages
+    end
   end
 
 end
