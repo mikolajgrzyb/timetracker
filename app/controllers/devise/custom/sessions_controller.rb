1c61d0956b468bcb9c87f5f1c80b60e42e195693
Mikołaj Grzyb <mikolaj@codequest.com>
2014-10-04 12:47:01 +0200

    add redirect when user don't have account (sign in)

--- app/controllers/devise/custom/sessions_controller.rb
+++ app/controllers/devise/custom/sessions_controller.rb
@@ -2,22 +2,22 @@ class Devise::Custom::SessionsController < Devise::SessionsController
 
   def create
     self.resource = warden.authenticate!(auth_options)
-    set_flash_message(:notice, :signed_in) if is_flashing_format?
-
     if resource.accounts.empty?
+      sign_out resource
       redirect_to new_user_session_path, alert: "Sorry, but you're not a member of any account. To sign in, you have to get an invitation to an account."
+    else
+      set_flash_message(:notice, :signed_in) if is_flashing_format?
+      sign_in(resource_name, resource)
+      yield resource if block_given?
+      respond_with resource, location: after_sign_in_path_for(resource)
     end
-
-    sign_in(resource_name, resource)
-    yield resource if block_given?
-    respond_with resource, location: after_sign_in_path_for(resource)
   end
 
   def after_sign_in_path_for(resource)
     if resource.first_account
       account_path(resource.first_account)
     else
-        root_path
+      root_path
     end
   end
 
