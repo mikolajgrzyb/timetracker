7393c611ec0d83b26f7eff0fdf302983e3746ca5
Mikołaj Grzyb <mikolaj@codequest.com>
2014-10-05 11:55:10 +0200

    add tests for for members Controller

--- spec/controllers/members_controller_spec.rb
+++ spec/controllers/members_controller_spec.rb
@@ -2,6 +2,26 @@ require 'rails_helper'
 
 RSpec.describe MembersController, :type => :controller do
 
+  describe "GET #index" do
+    let(:user) { create :user }
+    let(:user2) { create :user }
+
+    let(:account) { create :account }
+    let!(:member) { create :member, user: user, account: account, role: 'owner', active: true }
+    let!(:member1) { create :member, user: user2, account: account, role: 'regular', active: false }
+
+    before do
+      sign_in(user)
+    end
+    it 'assigns variables' do
+      get :index, account_id: account.id
+      expect(assigns(:active_members)).to eq [member]
+      expect(assigns(:inactive_members)).to eq [member1]
+      expect(assigns(:invitation)).to be_new_record
+
+    end
+  end
+
   describe "PUT #update" do
     let(:user) { create :user }
 
@@ -36,7 +56,15 @@ RSpec.describe MembersController, :type => :controller do
     end
 
     context 'when member can update' do
-      let(:user2) {create :user }
+      let(:user2) { create :user }
+      let(:member2) { create :member, user: user2, account: account, role: 'regular' }
+      it 'updates record' do
+        put :update, account_id: account.id, id: member2, member: {active: false, role: 'admin'}
+        member2.reload
+        expect(member2.active).to eq false
+        expect(member2.role).to eq 'admin'
+
+      end
 
     end
   end
