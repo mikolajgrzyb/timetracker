2108ff3cce398554b11abea7613991cb7c256609
Andrzej Trzaska <atrzaska2@gmail.com>
2014-10-05 11:51:28 +0200

    add account for each user, modify user_spec

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,13 +3,11 @@ require 'rails_helper'
 describe User, type: :model do
 
   let(:user) { create(:user) }
-  let(:account) { create(:account) }
 
   describe "#first_account" do
 
     it "returns first account for user" do
-      user.accounts << account
-      expect(user.first_account).to eq account
+      expect(user.first_account).to eq user.accounts.first
     end
 
   end
